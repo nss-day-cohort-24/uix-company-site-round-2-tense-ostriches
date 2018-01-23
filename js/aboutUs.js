2bc94c07f4ef0b29480ed6a13b42162bc3f00c74
Phonethip <phonethip.liu@gmail.com>
2018-01-23 14:26:10 -0600

    ph updates to about page

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -8,14 +8,14 @@ Person.prototype.badge = function() {
     return "<h3>" + this.firstName + " " + this.lastName + "</h3>" + "<p>" + this.title + "<br>" + this.phone + "</p>"
 };
 
-var staffThip = new Person("Phonethip", "Hobson", "Fit-U team lead", 888-567-8910);
+var staffThip = new Person("Phonethip", "Hobson", "Fit-U team lead", "888-567-8910");
 document.getElementById("thip").innerHTML = staffThip.badge();
 
-var staffMeg = new Person("Meg", "Scholl", "Fit-U CSS Workout Guru", 888-567-8911);
+var staffMeg = new Person("Meg", "Scholl", "Fit-U CSS Workout Guru", "888-567-8911");
 document.getElementById("meg").innerHTML = staffMeg.badge();
 
-var staffMillion = new Person("Million", "Asseghegn", "Fit-U HTML Dietitian ", 888-567-8912);
+var staffMillion = new Person("Million", "Asseghegn", "Fit-U HTML Dietitian ", "888-567-8912");
 document.getElementById("million").innerHTML = staffMillion.badge();
 
-var staffRyan = new Person("Ryan", "Brisentine", "Fit-U JS Life Coach", 888-567-8913);
+var staffRyan = new Person("Ryan", "Brisentine", "Fit-U JS Life Coach", "888-567-8913");
 document.getElementById("ryan").innerHTML = staffRyan.badge();
